8e5b36901a38cf5ce8c82882783158f7721c441b
Anurag Gade <88252632+Anurag-Gade@users.noreply.github.com>
2024-07-30 00:41:32 +0530

    Create 6 - Adder100i.v

---
+++ Verilog Language/5 - More Verilog Features/6 - Adder100i.v
@@ -0,0 +1,32 @@
+module one_bit_adder(input a, 
+                     input b,
+                     input cin,
+                     output sum, 
+                     output cout);
+    
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | cin & (a ^ b);
+    
+endmodule
+
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+
+    genvar i;
+    one_bit_adder zero_bit(.a(a[0]), .b(b[0]), .cin(cin), .cout(cout[0]), .sum(sum[0]));
+    
+    generate 
+        
+        for (i = 1; i <=99; i = i + 1) begin: adder_blocks
+            
+            one_bit_adder(.a(a[i]), .b(b[i]), .cin(cout[i - 1]), .cout(cout[i]), .sum(sum[i]));
+            
+        end
+        
+    endgenerate
+        
+        
+endmodule
